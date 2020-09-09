48f9e73d4d3e57f27cdc0bc38a8716a21e0caadd
MetaBarj0 <troctsch.cpp@gmail.com>
2020-09-09 09:34:39 +0000

    Applicative power with validation
    - gotten rid of monads
    - better error messages

--- src/Validation.hs
+++ src/Validation.hs
@@ -4,32 +4,16 @@ module Validation
 where
 
 import Data.Char (isAlphaNum, isSpace)
+import Data.Validation
 
-makeUser :: String -> String -> Either Error User
-makeUser name password =
-  User
-    <$> validateName name
-    <*> validatePassword password
+newtype Error = Error {errorAsStringList :: [String]} deriving (Show)
 
-validateName :: String -> Either Error Name
-validateName name =
-  stripSpace name
-    >>= isOnlyAlphaNum
-    >>= (Right . Name)
-    >>= nameMustHaveValidLength
-
-validatePassword :: String -> Either Error Password
-validatePassword password =
-  stripSpace password
-    >>= isOnlyAlphaNum
-    >>= (Right . Password)
-    >>= passwordMustHaveValidLength
-
-newtype Password = Password {passwordAsString :: String} deriving (Show)
-
-newtype Error = Error {errorAsString :: String} deriving (Show)
+instance Semigroup Error where
+  Error left <> Error right = Error $ left <> right
 
 newtype Name = Name {nameAsString :: String} deriving (Show)
+
+newtype Password = Password {passwordAsString :: String} deriving (Show)
 
 data User
   = User
@@ -38,24 +22,50 @@ data User
       }
   deriving (Show)
 
-passwordMustHaveValidLength :: Password -> Either Error Password
+makeUser :: String -> String -> Validation Error User
+makeUser name password =
+  User
+    <$> validateName name
+    <*> validatePassword password
+
+data InputType = NameInput | PasswordInput
+
+inputTypeAsString :: InputType -> String
+inputTypeAsString inputType =
+  case inputType of
+    NameInput -> "name"
+    PasswordInput -> "password"
+
+validateName :: String -> Validation Error Name
+validateName name =
+  stripSpace name NameInput
+    *> isOnlyAlphaNum name NameInput
+    *> nameMustHaveValidLength (Name name)
+
+validatePassword :: String -> Validation Error Password
+validatePassword password =
+  stripSpace password PasswordInput
+    *> isOnlyAlphaNum password PasswordInput
+    *> passwordMustHaveValidLength (Password password)
+
+passwordMustHaveValidLength :: Password -> Validation Error Password
 passwordMustHaveValidLength password
-  | length (passwordAsString password) > 20 = Left $ Error "The password must not have more than 20 characters"
-  | otherwise = Right password
+  | length (passwordAsString password) > 20 = Failure $ Error ["The password must not have more than 20 characters"]
+  | otherwise = Success password
 
-isOnlyAlphaNum :: String -> Either Error String
-isOnlyAlphaNum password
-  | not $ all isAlphaNum password = Left $ Error "The specified input must not contain any non alphanumerical characters"
-  | otherwise = Right password
+isOnlyAlphaNum :: String -> InputType -> Validation Error String
+isOnlyAlphaNum input inputType
+  | not $ all isAlphaNum input = Failure $ Error ["The specified " ++ inputTypeAsString inputType ++ " must not contain any non alphanumerical characters"]
+  | otherwise = Success input
 
-stripSpace :: String -> Either Error String
-stripSpace [] = Left $ Error "The specified input must not be empty"
-stripSpace (x : xs) = if isSpace x then stripSpace xs else Right $ stripReversed $ reverse $ x : xs
+stripSpace :: String -> InputType -> Validation Error String
+stripSpace [] inputType = Failure $ Error ["The specified " ++ inputTypeAsString inputType ++ " must not be empty"]
+stripSpace (x : xs) inputType = if isSpace x then stripSpace xs inputType else Success $ stripReversed $ reverse $ x : xs
   where
     stripReversed :: String -> String
     stripReversed (x : xs) = if isSpace x then stripReversed xs else reverse (x : xs)
 
-nameMustHaveValidLength :: Name -> Either Error Name
+nameMustHaveValidLength :: Name -> Validation Error Name
 nameMustHaveValidLength name
-  | length (nameAsString name) > 15 = Left $ Error "The user name must not have more than 15 characters"
-  | otherwise = Right name
+  | length (nameAsString name) > 15 = Failure $ Error ["The user name must not have more than 15 characters"]
+  | otherwise = Success name
